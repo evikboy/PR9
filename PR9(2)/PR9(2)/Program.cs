2d48ee6ceb1d9dfae7484f574650315be2dea642
evikboy <Kislenko1950@gmail.com>
2021-04-28 14:33:15 +0300

    PR9

---
+++ PR9(2)/PR9(2)/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR7
+{
+
+    class ship
+    {
+        protected string name;
+        protected string appointment;
+        protected double cavity;
+        protected double enginePower;
+        protected string nameFuel;
+
+        virtual public void input()
+        {
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Назначение: ");
+            appointment = Console.ReadLine();
+            Console.Write("Водоизмещение(в тоннах): ");
+            cavity = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Мощность двигателя(кВт): ");
+            enginePower = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Вид горючего: ");
+            nameFuel = Console.ReadLine();
+        }
+
+        virtual public void output()
+        {
+            Console.WriteLine("Имя: {0}\nНазначение: {1}\nВодоизмещение: {2}\nМощность двигателя: {3}\nВид горючего: {4}",
+                name, appointment, cavity, enginePower, nameFuel);
+        }
+    }
+
+    class carrier : ship
+    {
+        string typePlane;
+        int numberPlanes;
+
+        public override void input()
+        {
+            base.input();
+
+            Console.Write("Тип самолетов, которые он везет: ");
+            typePlane = Console.ReadLine();
+            Console.Write("Их количество: ");
+            numberPlanes = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public override void output()
+        {
+            Console.WriteLine("\n======================АВИАНОСЕЦ======================");
+            base.output();
+
+            Console.WriteLine("Тип самолетов, которые он везет: {0}\nИх количество: {1}", typePlane, numberPlanes);
+        }
+    }
+
+    class rocketCarrier : ship
+    {
+        string typeRockets;
+        int numberRockets;
+
+        public override void input()
+        {
+            base.input();
+
+            Console.Write("Тип ракет: ");
+            typeRockets = Console.ReadLine();
+            Console.Write("Их количество: ");
+            numberRockets = Convert.ToInt32(Console.ReadLine());
+        }
+        public override void output()
+        {
+            Console.WriteLine("\n======================РАКЕТОНОСЕЦ======================");
+            base.output();
+
+            Console.WriteLine("Тип ракет: {0}\nИх количество: {1}", typeRockets, numberRockets);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Заполните все данные про авианосец");
+            carrier carrier = new carrier();
+            carrier.input();
+            carrier.output();
+            Console.WriteLine();
+            Console.WriteLine("Заполните все данные про ракетоносец");
+            rocketCarrier rocketCarrier = new rocketCarrier();
+            rocketCarrier.input();
+            rocketCarrier.output();
+        }
+    }
+}
---
+++ PR9/PR9/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR9
+{
+    class trasport
+    {
+        protected double income;
+        virtual public void input()
+        {
+            Console.Write("Введите чистую прибыль от транспорта: ");
+            income = Convert.ToDouble(Console.ReadLine());
+        }
+
+        virtual public void output()
+        {
+            Console.WriteLine("Прибыль от транспорта: {0}", income);
+        }
+    }
+
+    class freightTransport : trasport
+    {
+        double lifting;
+        double horsepower;
+        double volume;
+
+        public override void input()
+        {
+            Console.Write("Введите грузоподъемность грузового траспорта(тонны): ");
+            lifting = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите количество лошадиных сил: ");
+            horsepower = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите объем топливного бака(литры): ");
+            volume = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите чистую прибыль от грузового транспорта: ");
+            income = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public override void output()
+        {
+            Console.WriteLine("Грузоподъемность грузового траспорта: {0}\nКоличество лошадиных сил: {1}\nОбъем топливного бака: {2}", lifting, horsepower, volume);
+            base.output();
+        }
+
+    }
+
+    class passengerTrasport : trasport
+    {
+        int passCapacity;
+        int comfort;
+        double price;
+
+        public override void input()
+        {
+            Console.Write("Введите пассажировместимость: ");
+            passCapacity = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите цену билета: ");
+            price = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите баллы комфорта(1-10): ");
+            comfort = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите чистую прибыль от пассажирского транспорта: ");
+            income = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public override void output()
+        {
+            Console.WriteLine("Пассажировместимость: {0}\nЦена билета: {1}\nБаллы комфорта: {2}", passCapacity, price, comfort);
+            base.output();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            freightTransport freightTransport = new freightTransport();
+            Console.WriteLine("Введите все данные про грузовой транспорт");
+            freightTransport.input();
+            Console.WriteLine("\n================================ГРУЗОВОЙ ТРАНСПОРТ==================================");
+            freightTransport.output();
+
+            Console.WriteLine();
+
+            passengerTrasport passengerTrasport = new passengerTrasport();
+            Console.WriteLine("Введите все данные про пассажирский транспорт");
+            passengerTrasport.input();
+            Console.WriteLine("\n================================ПАССАЖИРСКИЙ ТРАНСПОРТ==================================");
+            passengerTrasport.output();
+        }
+    }
+}
